eb63fa12c079ee908d88b0ee97bec84de7652474
aim467 <709544711@qq.com>
2023-04-20 23:56:55 +0800

    init repo

---
+++ DataNexus-Backend/src/main/java/com/root2z/data/nexus/DataNexusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.root2z.data.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataNexusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataNexusBackendApplication.class, args);
+    }
+
+}
---
+++ DataNexus-Backend/src/test/java/com/root2z/data/nexus/DataNexusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root2z.data.nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataNexusBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
